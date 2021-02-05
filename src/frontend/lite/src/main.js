567b8545196c52a13793cb2b42444ec573c192ee
Vincent van der Kroef <devlz303@gmail.com>
2021-02-05 14:01:32 +0100

    remove vue-js-toggle-button move i18n to plugins filder

--- src/frontend/lite/src/main.js
+++ src/frontend/lite/src/main.js
@@ -1,15 +1,12 @@
 import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
-import router from "./router";
-import i18n from "./i18n";
+import App from "@/App.vue";
+import store from "@/store";
+import router from "@/router";
 
 import "@/components";
 
 import "@/plugins/fontawesome";
-
-import ToggleButton from "vue-js-toggle-button";
-Vue.use(ToggleButton);
+import i18n from "@/plugins/i18n";
 
 import PortalVue from "portal-vue";
 Vue.use(PortalVue);
