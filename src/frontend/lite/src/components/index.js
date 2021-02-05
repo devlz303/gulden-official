ef60be83122782810f3ad21a71178098448ecef2
Vincent van der Kroef <devlz303@gmail.com>
2021-02-05 14:34:14 +0100

    remove select-list component

--- src/frontend/lite/src/components/index.js
+++ src/frontend/lite/src/components/index.js
@@ -4,13 +4,11 @@ import ClipboardField from "./ClipboardField";
 import GuldenButtonSection from "./layout/GuldenButtonSection";
 import GuldenFormField from "./layout/GuldenFormField";
 import GuldenSection from "./layout/GuldenSection";
-import SelectList from "./SelectList";
 
 Vue.component(ClipboardField.name, ClipboardField);
 Vue.component(GuldenButtonSection.name, GuldenButtonSection);
 Vue.component(GuldenFormField.name, GuldenFormField);
 Vue.component(GuldenSection.name, GuldenSection);
-Vue.component(SelectList.name, SelectList);
 
 import FontAwesomeIcon from "./FontAwesomeIcon";
 Vue.component("fa-icon", FontAwesomeIcon);
