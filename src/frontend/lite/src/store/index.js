4c753fc097304e30c8e671e211007c7fb410958d
Vincent van der Kroef <devlz303@gmail.com>
2021-02-08 16:43:24 +0100

    refactor vuex store add vuex logger in development

--- src/frontend/lite/src/store/index.js
+++ src/frontend/lite/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 import { createPersistedState, createSharedMutations } from "vuex-electron";
 import Store from "electron-store";
 import syncState from "./syncState";
@@ -8,19 +9,26 @@ import walletPath from "@/walletPath";
 
 Vue.use(Vuex);
 
-let userSettingsPersistedState = () => {};
-// create persistate state in main process only
-if (process.type !== "renderer") {
-  // use user-settings.json file to store settings in wallet folder
-  let store = new Store({ cwd: walletPath, name: "user-settings" });
+const plugins = [syncState, createSharedMutations()];
 
-  userSettingsPersistedState = createPersistedState({
-    storage: store,
-    invertIgnored: true, // use invertIgnored because we only want to store the paths in the ignoredPaths list
-    ignoredPaths: ["app.theme"]
-  });
+const isDevelopment = process.env.NODE_ENV !== "production";
+// add vuex logger when debugging and only in renderer
+if (isDevelopment && process.type === "renderer") {
+  plugins.push(createLogger());
 }
+
+// add persistate state in main process only
+if (process.type !== "renderer") {
+  plugins.push(
+    createPersistedState({
+      storage: new Store({ cwd: walletPath, name: "user-settings" }), // use user-settings.json file to store settings in wallet folder
+      invertIgnored: true, // use invertIgnored because we only want to store the paths in the ignoredPaths list
+      ignoredPaths: ["app.theme"] // <-- these properies will be stored in user-settings.json
+    })
+  );
+}
+
 export default new Vuex.Store({
   modules: modules,
-  plugins: [syncState, userSettingsPersistedState, createSharedMutations()]
+  plugins: plugins
 });
---
+++ src/frontend/lite/src/store/modules/app.store.js
@@ -0,0 +1,104 @@
+import { Menu } from "electron";
+import AppStatus from "@/AppStatus";
+
+function EnableDebugWindowOnCoreReady() {
+  try {
+    let menu = Menu.getApplicationMenu();
+    if (menu === null) return;
+    menu.items
+      .find(x => x.label === "Help")
+      .submenu.items.find(x => x.label === "Debug window").enabled = true;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const state = {
+  coreReady: false,
+  language: null,
+  rate: null,
+  splashReady: false,
+  status: AppStatus.start,
+  theme: null,
+  unityVersion: null,
+  walletExists: null,
+  walletVersion: null
+};
+
+const getters = {};
+
+const actions = {
+  SET_CORE_READY({ commit, state }) {
+    commit(
+      "SET_STATUS",
+      state.progress === 1 ? AppStatus.ready : AppStatus.synchronize
+    );
+    commit("SET_CORE_READY");
+    EnableDebugWindowOnCoreReady();
+  },
+  SET_LANGUAGE({ commit }, language) {
+    commit("SET_LANGUAGE", language);
+  },
+  SET_RATE({ commit }, rate) {
+    commit("SET_RATE", rate);
+  },
+  SET_SPLASH_READY({ commit }) {
+    commit("SET_SPLASH_READY");
+  },
+  SET_STATUS({ commit }, status) {
+    commit("SET_STATUS", status);
+  },
+  SET_THEME({ commit }, theme) {
+    commit("SET_THEME", theme);
+  },
+  SET_UNITY_VERSION({ commit }, version) {
+    commit("SET_UNITY_VERSION", version);
+  },
+  SET_WALLET_EXISTS({ commit }, walletExists) {
+    let status = walletExists ? AppStatus.synchronize : AppStatus.setup;
+    commit("SET_STATUS", status);
+    commit("SET_WALLET_EXISTS", walletExists);
+  },
+  SET_WALLET_VERSION({ commit }, version) {
+    commit("SET_WALLET_VERSION", version);
+  }
+};
+
+const mutations = {
+  SET_CORE_READY(state) {
+    state.coreReady = true;
+  },
+  SET_LANGUAGE(state, language) {
+    state.language = language;
+  },
+  SET_RATE(state, rate) {
+    state.rate = rate;
+  },
+  SET_SPLASH_READY(state) {
+    state.splashReady = true;
+  },
+  SET_STATUS(state, status) {
+    if (state.status === AppStatus.shutdown) return; // shutdown in progress, do not switch to other status
+    state.status = status;
+  },
+  SET_THEME(state, theme) {
+    state.theme = theme;
+  },
+  SET_UNITY_VERSION(state, version) {
+    state.unityVersion = version;
+  },
+  SET_WALLET_EXISTS(state, walletExists) {
+    state.walletExists = walletExists;
+  },
+  SET_WALLET_VERSION(state, version) {
+    state.walletVersion = version;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
